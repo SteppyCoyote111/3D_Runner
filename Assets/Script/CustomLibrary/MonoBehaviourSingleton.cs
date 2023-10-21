41dfee963971f7b901710a8183aff5a84c703d66
SteppyCoyote111 <92569647+SteppyCoyote111@users.noreply.github.com>
2023-10-21 22:05:54 +0300

    added baseData

---
+++ Assets/Script/CustomLibrary/MonoBehaviourSingleton.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public abstract class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindExistingInstance() ?? CreateNewInstance();
+            }
+            
+            return _instance;
+        }
+    }
+
+    private static T CreateNewInstance()
+    {
+        var singletonObject = new GameObject {name = $"New {typeof(T)} (Singleton)"};
+        return singletonObject.AddComponent<T>();
+    }
+
+    private static T FindExistingInstance()
+    {
+        return GameObject.FindObjectOfType<T>(true);
+    }
+
+    private static void Enable()
+    {
+        if (_instance != null)
+        {
+            _instance.enabled = true;
+        }
+    }
+
+    private void Awake()
+    {
+        var component = GetComponent<T>();
+        
+        if (_instance == null)
+        {
+            _instance = component;
+        }
+        else if (_instance != component)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        Enable();
+        Initialize();
+    }
+
+    protected virtual void Initialize()
+    {
+    }
+}
---
+++ Assets/Script/Data/BaseData.cs
@@ -0,0 +1,9 @@
+﻿    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BaseData", order = 1)]
+    public class BaseData : ScriptableObject
+    {
+        public Vector2 LeftPoint = new Vector2(-3, 0);
+        public Vector2 MiddlePoint =  new Vector2(0, 0);
+        public Vector2 RightPoint = new Vector2(3, 0);
+    }
